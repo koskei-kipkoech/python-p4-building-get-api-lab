36f57231cd9387fd9418b62bae6a1c6273bf1228
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-17 17:01:55 +0300

    updates changes on server

--- server/app.py
+++ server/app.py
@@ -20,19 +20,29 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    result = [bakery.to_dict() for bakery in bakeries]
+    return result
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = db.session.get(Bakery,id)
+    return bakery.to_dict()
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    result = [good.to_dict() for good in baked_goods]
+    return result
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        return most_expensive.to_dict()  
+    else:
+        return make_response({"Error": "No baked goods found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
